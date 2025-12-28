b537d811ff9280bba47b779373deec92843cd632
Jesusalcudia12 <132318909+Jesusalcudia12@users.noreply.github.com>
2025-12-27 21:43:33 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 # Pyrogram setup
 API_ID = "36863635"  # Replace this API ID with your actual API ID
-API_HASH = "
-eaad5e9e1f20e047daee4997e88814d6"  # Replace this API HASH with your actual API HASH
+API_HASH = "eaad5e9e1f20e047daee4997e88814d6"  # Replace this API HASH with your actual API HASH
 SESSION_STRING = "AQIyfpMAwEvxjwvAgaUJcufsJAehuwqGqMy8kKDQH4zT-195pfgAkJRYnpv7aP37F2kJPQhM0L6AFFzG9ZTa8SiX1YXYZGARMY_kRo9NiQQLZ4_XFdOYevPcIzNlVVlp4FidvacYmht120JtsWABMGF_ygnLUCJOng8DDDfZJGZoxmkBg6OAUYSjiz6R_AUNaqjD1Z7YdKCGKzGsap8WEDVFrDUc7QgOcx4l52qLHnporiRNmxZwHPmhsinXyG0GIwwguNTk9npLWiBmxmyPzD9MX8Vloi1SVXXUQhxGYonb7H0de1LuiEYIjRX0vWCaYgOTfVbF2t5iadDodZR7fhdep9cbYgAAAAHcdoVmAA"  # Replace this SESSION STRING with your actual SESSION_STRING
 BOT_TOKEN = "8255731465:AAGrPR_qH0zC4zTdBqOj-Zt1jqY0W03-yws"  # Replace this BOT_TOKEN
 
@@ -12,6 +11,7 @@ ADMIN_IDS = [7993722214]
 DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
 
 ADMIN_LIMIT = 50000 # Card Scrapping Limit For Admin
+
 
 
 
