598b4fcb948e8903ce9d4338493ec6f2a47c3a6e
Jesusalcudia12 <132318909+Jesusalcudia12@users.noreply.github.com>
2025-12-27 20:51:55 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # Pyrogram setup
-API_ID = "123456"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
+API_ID = "36863635"  # Replace this API ID with your actual API ID
+API_HASH = "
+eaad5e9e1f20e047daee4997e88814d6"  # Replace this API HASH with your actual API HASH
 SESSION_STRING = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Replace this SESSION STRING with your actual SESSION_STRING
-BOT_TOKEN = "8583960709:AAGMxsIwVzlVUu-YvSn6Rfxn3-2Vfe-T3WU"  # Replace this BOT_TOKEN
+BOT_TOKEN = "8255731465:AAGrPR_qH0zC4zTdBqOj-Zt1jqY0W03-yws"  # Replace this BOT_TOKEN
 
 # Admin IDs
 ADMIN_IDS = [12345678, 12345678]
@@ -11,3 +12,4 @@ ADMIN_IDS = [12345678, 12345678]
 DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
 
 ADMIN_LIMIT = 50000 # Card Scrapping Limit For Admin
+
